8196c1956f790fbb1ec46e59e4d5fa2f472566c7
pascal07_2025 <pascal.wundsam.aps@gmail.com>
2025-08-18 00:30:29 +0200

    Update __init__.py

--- novacordpy/__init__.py
+++ novacordpy/__init__.py
@@ -1,7 +1,7 @@
 __title__ = "NovaCordpy"
 __author__ = "atpascal07"
 __license__ = "MIT"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 
 from . import blacklist, components, emb, i18n, logs, sql, times, utils
 from .bot import *
